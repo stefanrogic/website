d445537268108c33b135c8c673a12cb0c508a29b
stefanrogc <stefanrogic@protonmail.com>
2024-03-25 15:09:45 +0100

    scapped old design

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
 import "./App.scss";
-import LeftPanel from "./components/leftPanel/LeftPanel";
-import RightPanel from "./components/rightPanel/RightPanel";
-import AboutPage from "./pages/aboutPage/AboutPage";
-import ContactPage from "./pages/contactPage/ContactPage";
-import HomePage from "./pages/homePage/HomePage";
-import ProjectsPage from "./pages/projectsPage/ProjectsPage";
 
 function App() {
-  const getUrl = (fileName) => new URL(`/public/${fileName}`, import.meta.url).href;
+  // const getUrl = (fileName) => new URL(`/public/${fileName}`, import.meta.url).href;
 
-  return (
-    <div className="App">
-      <button className="menu-btn">Menu</button>
-      <LeftPanel />
-      <div className="content">
-        <HomePage />
-        <ProjectsPage />
-        <AboutPage getUrl={getUrl} />
-        <ContactPage />
-      </div>
-      <RightPanel />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
