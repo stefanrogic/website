3fcf9c070322caa9376f87b89414e25fba81671e
stefanrogic <Stefanrogic@protonmail.com>
2024-06-13 18:07:54 +0200

    fixed date for education

--- src/components/educationSection/EducationSection.jsx
+++ src/components/educationSection/EducationSection.jsx
@@ -23,7 +23,7 @@ const EducationSection = () => {
 
             <div className="card-right">
               <p>
-                <i>2019-2022 October</i>
+                <i>2019-October 2022</i>
               </p>
             </div>
           </div>
